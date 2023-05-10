d67041d34d6cbbaa5b1254541efb4fdc383a9d1c
la790x <la790x@gmail.com>
2023-05-10 15:13:27 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if ('local' == config('app.env')) {
+            \App\Models\User::factory()->create([
+                'name' => 'latief',
+                'email' => 'latif@visiglobalteknologi.co.id',
+                'password' => bcrypt('12345678')
+            ]);
+        }
     }
 }
