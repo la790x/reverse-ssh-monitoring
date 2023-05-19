2f3de854674c253e391d34480ceaafb0f1b24993
la790x <la790x@gmail.com>
2023-05-19 10:52:53 +0700

    fix dikit

--- database/migrations/2023_05_09_132527_create_devices_table.php
+++ database/migrations/2023_05_09_132527_create_devices_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('devices', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('unique_code')->unique();
             $table->text('description')->nullable();
             $table->unsignedBigInteger('client_id');
             $table->timestamps();
